c2765d062f53a16a1b378e360f6ae4db472ced4d
Juan David <juandominguez2017@u.northwestern.edu>
2015-07-13 23:19:18 -0700

    Changed header to better reflect contributors

--- scripts/components/Header.react.jsx
+++ scripts/components/Header.react.jsx
@@ -44,7 +44,7 @@ var Header = React.createClass({
       <nav className="top-bar" data-topbar role="navigation">
         <ul className="title-area">
           <li className="name">
-            <h1><a href="#"><strong>Super Class</strong>, by Juan David</a></h1>
+            <h1><a href="#"><strong>Super Class</strong>, by JDD, MH & BS</a></h1>
           </li>
           <li className="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         </ul>
