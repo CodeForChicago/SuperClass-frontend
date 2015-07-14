a6e791008e19b48c9ecef68d1a423c938798d7b1
Juan David <juandominguez2017@u.northwestern.edu>
2015-07-13 23:50:16 -0700

    Fixed the missing </ul> bug at the end.

--- scripts/components/questions/QuestionsPage.react.jsx
+++ scripts/components/questions/QuestionsPage.react.jsx
@@ -49,7 +49,7 @@ var QuestionItem = React.createClass({
   render: function() {
     < li className="question">
       <div className="question__title">
-        <Link to="question" params={ {lessonId: this.props.question.id} }>
+        <Link to="question" params={ {questionId: this.props.question.id} }>
           {this.props.question.title}
         </Link>
       </div>
@@ -61,7 +61,7 @@ var QuestionItem = React.createClass({
       </div>
     </li>
   }
-})
+});
 
 var QuestionsList = React.createClass({
   render: function() {
@@ -70,6 +70,7 @@ var QuestionsList = React.createClass({
         {this.props.questions.map(function(question, index){
           return <QuestionItem question={question} key={"question-" + index}/>
         })}
+        </ul>
     );
   }
 });
