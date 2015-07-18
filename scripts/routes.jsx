bcb7e0f4e609505ea324b0c3c69b3e107a278a91
Juan David <juandominguez2017@u.northwestern.edu>
2015-07-17 21:24:49 -0700

    Getting file system prepared for a question page and a new question.

--- scripts/routes.jsx
+++ scripts/routes.jsx
@@ -9,7 +9,9 @@ var LoginPage = require('./components/session/LoginPage.react.jsx');
 var LessonsPage = require('./components/lessons/LessonsPage.react.jsx');
 var LessonPage = require('./components/lessons/LessonPage.react.jsx');
 var LessonNew = require('./components/lessons/LessonNew.react.jsx');
-var QuestionsPage = require('./components/questions/QuestionsPage.react.jsx')
+var QuestionsPage = require('./components/questions/QuestionsPage.react.jsx');
+var QuestionPage = require('./components/questions/QuestionPage.react.jsx');
+var QuestionNew = require('./components/questions/QuestionNew.react.jsx');
 
 module.exports = (
   <Route name="app" path="/" handler={SuperclassApp}>
@@ -20,7 +22,8 @@ module.exports = (
     <Route name="lesson" path="/lessons/:lessonId" handler={LessonPage}/>
     <Route name="new-lesson" path="/lesson/new" handler={LessonNew}/>
     <Route name="questions"  path="/questions" handler={QuestionsPage}/>
-    <Route name="question" path="/questions/:questionId" handler={QuestionsPage}/>
+    <Route name="question" path="/questions/:questionId" handler={QuestionPage}/>
+    <Route name="new-question" path"/question/new" handler={QuestionNew}/>
   </Route>
 );
 
