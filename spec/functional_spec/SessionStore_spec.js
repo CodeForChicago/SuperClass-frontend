12cadd6d10f9853865ed0798314a4b7ff3a8cb8b
Juan David <juandominguez2017@u.northwestern.edu>
2016-02-20 14:29:54 -0600

    Adds the sessionstorage necessary to run

--- spec/functional_spec/SessionStore_spec.js
+++ spec/functional_spec/SessionStore_spec.js
@@ -1,2 +1,14 @@
 var sessionStorageMock = require('../spec_helper.js').sessionStorageMock;
+
+sessionStorageMock.init();
 var SessionStore = require('../../scripts/stores/SessionStore.js');
+
+describe ("SessionStore", function() {
+	it("Does not have an individual logged in", function(done) {
+		expect(SessionStore.getEmail()).toEqual(null);
+		done();
+	});
+			
+});
+
+sessionStorageMock.destroy();
